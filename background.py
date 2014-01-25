98f6a6c7e20ea7c15768d95dd81dca2a572b992c
Du Lin <lnidux@gmail.com>
2014-01-25 15:27:52 +0100

    Background a scorrimento, piccolo bug

--- background.py
+++ background.py
@@ -1,35 +1,59 @@
 from imports import *
-from random import randint
-
+from random import randint		
+		
 class Background(pygame.sprite.Sprite):
 
 	def __init__(self, game):
 		pygame.sprite.Sprite.__init__(self)
 		self.game = game
+		#self.rect = self.game.get_rect()
 
 		self.image = pygame.Surface(self.game.rect.size).convert_alpha()
-		self.rect = self.image.get_rect()
 
-		num_tiles = 4
-		num_tiles_single = 3
+		# Numero dei file tipo tile
+		self.num_tiles = 4
+		self.num_tiles_single = 3
 
-		self.tile = list()
-		# for i in (0, self.num_tiles - 1):
-		self.tile.append(pygame.image.load("img/tile_pavim_fogna1.png").convert())
-		self.tile_rect = self.tile[0].get_rect()
-
-		self.tile.append(pygame.image.load("img/tile_pavim_fogna2.png").convert())
-		self.tile.append(pygame.image.load("img/tile_pavim_fogna3.png").convert())
+		# Lista di immagini tile
+		self.tiles = list()
+		
+		self.tiles.append(pygame.image.load("img/tile_pavim_fogna1.png").convert())
+		self.tiles.append(pygame.image.load("img/tile_pavim_fogna2.png").convert())
+		self.tiles.append(pygame.image.load("img/tile_pavim_fogna3.png").convert())
 
 		# Tile doppi:
-		# self.tile.append(pygame.image.load("img/tile_pavim_fogna_doppio.png").convert())
-		# self.tile_rect_double = self.tile[3].get_rect()
+		self.tiles.append(pygame.image.load("img/tile_pavim_fogna_doppio.png").convert())
 
-		for x in range(0, self.game.rect.w , self.tile_rect.w):
-			for y in range(0, self.game.rect.h, self.tile_rect.h):
-					
-					random_index = randint(0, num_tiles_single - 1)	# Per ora sono tile singoli.
-					self.image.blit( self.tile[random_index], (x,y) )
+		# rect di tile
+		self.side = self.tiles[0].get_rect().h
+
+		# offeset per aggiungere rows
+		self.offset = 0
+
+		# Lista di rows
+		self.rows = list()
+		for i in xrange(0, self.game.rect.h/self.side + 1):
+			self.rows.append(self.new_row())
+
+	# Metodo che crea rows secondo un indice random per i tiles
+	def new_row(self):
+		row = pygame.Surface((self.game.rect.w, self.side)).convert_alpha()
+		# row.fill((0, 0, 255))
+		position = -self.side/2
+		for i in xrange(0, self.game.rect.w/self.side + 1):
+			random_index = randint(0, self.num_tiles - 1)
+			row.blit(self.tiles[random_index], (position, 0))
+			position += self.tiles[random_index].get_rect().w
+
+		return row
 
 	def update(self):
-		None
+		self.offset += self.game.tick * self.game.speed
+		if (self.offset > self.side):
+			self.offset = 0
+			del self.rows[-1]
+			self.rows.insert(0, self.new_row())
+		for i in xrange(len(self.rows)):
+			self.image.blit(self.rows[i], (0, i * self.side + self.offset))
+
+		self.image.blit(self.rows[-1], (0, -self.side + self.offset))
