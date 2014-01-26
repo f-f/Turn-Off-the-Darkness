34341913c94aabbf650f0d19bc5eee6061a86dae
Du Lin <lnidux@gmail.com>
2014-01-26 11:03:34 +0100

    bg pulito

--- background.py
+++ background.py
@@ -1,6 +1,5 @@
 from imports import *		
 
-
 class Background(pygame.sprite.Sprite):
 
 	def __init__(self, game):
@@ -15,13 +14,14 @@ class Background(pygame.sprite.Sprite):
 		self.tiles_dungeon = self.create_tilelist("Dungeon")		
 		self.tiles_inferno = self.create_tilelist("Inferno")
 		
-		self.rows = list()
+		self.rows = list() # Lista di righe dello schermo
 		for i in xrange(0, self.game.rect.h/self.side + 1):
 			self.rows.append(self.new_row())
 		
 		self.black = False
 
 	def create_tilelist(self, livello):
+	# Ritorna una lista di tiles (pygame.image) caricati
 		self.tiles = list()
 		for i in xrange(0, 2):
 			self.tiles.append(pygame.image.load(tools.get_pathname(livello, "Pavimento", i)).convert())
@@ -30,20 +30,18 @@ class Background(pygame.sprite.Sprite):
 		return self.tiles
 
 	def new_row(self):
-
+	# Definisce una riga dello schermo con tiles random in base al livello e la ritorna
 		self.row = pygame.Surface((self.game.rect.w, self.side)).convert_alpha()
 		self.position = -self.side/2
-
-		self.fr = self.game.frenzy
+		fr = self.game.frenzy
 		
-
 		for i in xrange(0, self.game.rect.w/self.side + 1):
 			
 			random_index = random.randint(0, 4);
-			if (self.fr <= F_CALM):
+			if (fr <= F_CALM):
 				self.row.blit(self.tiles_fogna[random_index], (self.position, 0))
 				self.position += self.tiles_fogna[random_index].get_rect().w
-			elif (self.fr <= F_TRANS2):
+			elif (fr <= F_TRANS2):
 				self.row.blit(self.tiles_dungeon[random_index], (self.position, 0))
 				self.position += self.tiles_dungeon[random_index].get_rect().w
 			else:
@@ -51,13 +49,6 @@ class Background(pygame.sprite.Sprite):
 				self.position += self.tiles_inferno[random_index].get_rect().w			
 			
 		return self.row
-
-	def returntile(self):
-		if (random.randint(0,1) == 0):
-			self.tile = pygame.image.load(tools.get_pathname(self.livello, "Pav", random.randint(0,2))).convert() #self.load(self.livello, "Pav", random.randint(0,2))
-		else:
-			self.tile = pygame.image.load(tools.get_pathname(self.livello, "Pavimento", random.randint(0,2))).convert()
-		self.row.blit(self.tile, (self.position, 0))
 
 	def update(self):
 		if self.black:
